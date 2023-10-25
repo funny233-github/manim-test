c93ca17ae3dc07ffe192525a07a5df704078e7c6
funny_233 <83470506+funny233-github@users.noreply.github.com>
2023-10-25 15:26:19 +0800

    tree

--- myAnimation.py
+++ myAnimation.py
@@ -70,10 +70,72 @@ class Tree(Scene):
         base = Square().shift(DOWN*3).scale(1).rotate(rotate_angle)
         group = Group(base)
         self.play(ShowCreation(group))
-        for i in range(9):
+        for i in range(10):
             self.treeResult.append(base)
             self.grownBase(base,rotate_angle,Square().get_width(),PI/2,i)
             newgroup = Group(*self.treeResult)
             self.play(Transform(group,newgroup))
             self.wait()
-            self.treeREsult = []
+            self.treeResult = []
+class TreeBox(VMobject):
+    def facingLine(self,mobject:Mobject,width:float,facing:float):
+        x = UP * width/2 * np.sin(facing) 
+        y = RIGHT * width/2 * np.cos(facing)
+        line = Line(mobject.get_center(),mobject.get_center()+x+y)
+        self.add(line)
+    def createBranch(self,base:Mobject,baseFacing:float,width:float,theta:float=PI/2/2): 
+        center = base.get_center()
+        baseUP = UP * np.cos(baseFacing) - RIGHT * np.sin(baseFacing)
+        baseRIGHT = UP * np.sin(baseFacing) + RIGHT * np.cos(baseFacing)
+        leftWidth = np.cos(theta/2)*width
+        leftCenter = center + baseUP*((leftWidth*np.sin(theta/2+PI/4))/np.sqrt(2)+width/2) + baseRIGHT*(-width/2+leftWidth*np.cos(theta/2+PI/4)/np.sqrt(2))
+        leftBranch = base.deepcopy().rotate(theta/2).scale(leftWidth/width).move_to(leftCenter).set_color(BLUE)
+
+        rightWidth = np.sin(theta/2)*width
+        rightCenter = center + baseUP*(rightWidth*np.sin(3*PI/4-theta/2)/np.sqrt(2)+width/2) + baseRIGHT*(width/2-rightWidth*np.cos(3*PI/4-theta/2)/np.sqrt(2))
+        rightBranch = base.deepcopy().rotate((theta-PI)/2).scale(rightWidth/width).move_to(rightCenter).set_color(RED)
+        return leftBranch,rightBranch
+    def grownBase(self,base:Mobject,baseFacing:float,baseWidth:float,theta:float=PI/2/2,step:int=1):
+        leftBranch,rightBranch = self.createBranch(base,baseFacing,baseWidth,theta)
+        self.add(leftBranch)
+        self.add(rightBranch)
+        if step <= 1:
+            return
+        self.grownBase(leftBranch,theta/2+baseFacing,np.cos(theta/2)*baseWidth,theta,step-1)
+        self.grownBase(rightBranch,(theta-PI)/2+baseFacing,np.sin(theta/2)*baseWidth,theta,step-1)
+    def set_theta(self,theta:float=PI/2/2):
+        self.__init__(self.base,self.baseWidth,self.baseFacing,theta,self.step) 
+        return self
+        
+    def __init__(self,base:Mobject,baseWidth:float|None=None,baseFacing:float=0,theta:float=PI/2/2,step:int=1):
+        if baseWidth is None:
+            baseWidth = base.get_width()
+        self.base = base
+        self.baseWidth = baseWidth
+        self.baseFacing = baseFacing
+        self.theta = theta
+        self.step = step
+        super().__init__()
+        self.add(base)
+        self.grownBase(base,baseFacing,baseWidth,theta,step)
+class TestScene(Scene):
+    def construct(self):
+        DT = 1/60
+        tree = TreeBox(Square().shift(DOWN*2),step=10,theta=0)
+        self.add(tree)
+        for i in range(314):
+            tree.set_theta(i/100)
+            self.wait(DT)
+            print(i)
+class TestNumberScene(Scene):
+    def construct(self):
+        num = DecimalNumber().set_value(0).rotate(PI)
+        tree = TreeBox(Square().shift(DOWN*2),step=1,theta=0.1)
+        self.add(num,tree)
+        now = self.time
+        def numUpdater(m):
+            m.set_value(self.time-now)
+        num.add_updater(numUpdater)
+        f_always(tree.set_theta,num.get_value)
+        self.wait(PI)
+        num.remove_updater(numUpdater)
